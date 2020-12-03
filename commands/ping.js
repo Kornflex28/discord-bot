cb9a8022093928a804bbe42ddbb071b9e900423b
unknown <chauvet.louis@gmail.com>
2020-12-03 14:18:51 +0100

    fix typo

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,18 @@
+const Discord = require('discord.js');
 module.exports = {
 	name: 'ping',
     description: 'Ping!',
     cooldown: 1,
 	execute(message, args) {
 		var ping = Date.now() - message.createdTimestamp + " ms";
-		message.channel.send(`🏓 Pong !\n*ton ping: ${ping}*`);
+		const pingEmbed = new Discord.MessageEmbed()
+                    .setColor('RANDOM')
+					.setTitle('🏓 Pong')
+					.addFields(
+						{name:':timer: Ton ping',value:`${Date.now() - message.createdTimestamp} ms`,inline:true},
+						{name:':heartbeat: Mon ping',value:`${message.client.ws.ping} ms`,inline:true}
+					)
+					.setTimestamp()
+		message.channel.send(pingEmbed);
 	},
 };
