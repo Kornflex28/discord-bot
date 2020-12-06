4e1a5963e6f0916c5b6acaa3f65863cc56e003a6
unknown <chauvet.louis@gmail.com>
2020-12-06 17:03:56 +0100

    new commands

--- commands/word.js
+++ commands/word.js
@@ -7,16 +7,18 @@ module.exports = {
 	name: 'word',
     description: 'Lance les dés et obtiens un mot français aléatoire et sa définition',
     aliases:['mot'],
-	execute(message, args) { 
+	execute(message, args) {
+        message.channel.send('J\'ouvre mon dictionnaire...')
         fetch(dict_url+process.env.DICO_TOKEN).then(response => response.json())
         .then(json => {
-            // console.log(json);
+            console.log(json);
             return json[0].mot
         })
         .then(word => {
             fetch("https://api.dicolink.com/v1/mot/"+`${word}/definitions?limite=3&source=larousse&api_key=`+process.env.DICO_TOKEN).then(response => response.json())
             .then(json => {
-                // console.log(json)
+                console.log(json)
+                if (json.error){return message.reply('désolé mais mon dictionnaire s\'est déchiré on dirait... Réessaye si tu veux')}
                 var def_str = new Discord.MessageEmbed()
                             .setTitle(word)
                             .setDescription('[Larousse](https://www.larousse.fr/)')
