697b503be88326666c66b093edaf6519e1d7a22d
Kornflex28 <chauvet.louis@gmail.com>
2021-05-14 12:21:40 +0200

    stats command update

--- commands/stats.js
+++ commands/stats.js
@@ -4,7 +4,7 @@ const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
 const Usercommands = require("../database/uc.js");
 Usercommands.setURL(process.env.LEVELS_DB_URL);
 
-const excludedCommands = ['reload','stats'];
+const excludedCommands = ['reload', 'stats'];
 module.exports = {
     name: 'stats',
     description: 'Quelques infos sur les échanges entre moi-même et le serveur',
@@ -20,11 +20,11 @@ module.exports = {
             let userCommandsCount = 0
             for (let [com, value] of userGuild.commands) {
                 if (!excludedCommands.includes(com)) {
-                    if (!totalGuildCommandsCount.has(com)) { totalGuildCommandsCount.set(com, [value,[[userGuild.userID,value]]]) }
-                    else { 
-                        [old_val,userList] = totalGuildCommandsCount.get(com);
-                        userList.push([userGuild.userID,value])
-                        totalGuildCommandsCount.set(com, [value + old_val, userList]) 
+                    if (!totalGuildCommandsCount.has(com)) { totalGuildCommandsCount.set(com, [value, [[userGuild.userID, value]]]) }
+                    else {
+                        [old_val, userList] = totalGuildCommandsCount.get(com);
+                        userList.push([userGuild.userID, value])
+                        totalGuildCommandsCount.set(com, [value + old_val, userList])
                     }
                     userCommandsCount += value
                 }
@@ -47,20 +47,43 @@ module.exports = {
         const bg = {
             id: 'custom_canvas_background_color',
             beforeDraw: (chart) => {
-                const ctx = chart.canvas.getContext('2d');
+                var ctx = chart.canvas.getContext('2d');
                 ctx.save();
                 ctx.globalCompositeOperation = 'destination-over';
                 ctx.fillStyle = 'white';
                 ctx.fillRect(0, 0, chart.width, chart.height);
                 ctx.restore();
+                if (chart.config.centerText){
+                if (chart.config.centerText.display !== null &&
+                    typeof chart.config.centerText.display !== 'undefined' &&
+                    chart.config.centerText.display) {
+                    var width = chart.chart.width,
+                        height = chart.chart.height,
+                        ctx = chart.chart.ctx;
+
+                    ctx.restore();
+                    var fontSize = (height / 400).toFixed(2);
+                    ctx.font = fontSize + "em sans-serif";
+                    ctx.textBaseline = "middle";
+                    var text = chart.config.centerText.text,
+                        textX = Math.round((width - ctx.measureText(text).width) / 2),
+                        textY = chart.config.options.layout.padding + height / 2;
+
+                    ctx.fillText(text, textX, textY);
+                    ctx.save();
+                }}
             }
         };
 
-        barColor = 360*Math.random()
+        barColor = 360 * Math.random()
         let configuration;
         let canvasRenderService;
         if (!args.length) {
-            canvasRenderService = new ChartJSNodeCanvas({ width, height });
+            canvasRenderService = new ChartJSNodeCanvas({
+                width,
+                height,
+                plugins: { requireLegacy: ['chartjs-plugin-datalabels'] }
+            });
             configuration = {
                 type: 'bar',
                 data: {
@@ -68,149 +91,201 @@ module.exports = {
                     datasets: [{
                         label: 'Nombre d\'appel',
                         data: totalCommandsCounts_.map(x => x[1][0]),
-                        backgroundColor:`hsl(${barColor},100%,70%)`,
-                        borderColor:`hsl(${barColor},100%,25%)`,
+                        backgroundColor: `hsl(${barColor},100%,70%)`,
+                        borderColor: `hsl(${barColor},100%,25%)`,
                         borderWidth: 3,
-                        borderRadius:10,
+                        // borderRadius:10,
                     }]
                 },
                 plugins: [bg],
                 options: {
                     plugins: {
-                        title : {
-                            display: true,
-                            text:`Nombre d'appels par commande sur ${message.guild.name}`,
-                            font : {
-                                weight : 'bold',
-                                size: 40
-                            }
-                        },
-                        legend: {
-                            display: false
+                        datalabels: {
+                            backgroundColor: function (context) {
+                                col = context.dataset.backgroundColor.slice(0, context.dataset.backgroundColor.length - 4)
+                                col = col + '85%)';
+                                return col;
+                            },
+                            borderColor: function (context) {
+                                return context.dataset.borderColor;
+                            },
+                            borderRadius: 25,
+                            borderWidth: 2,
+                            color: function (context) {
+                                return context.dataset.borderColor;
+                            },
+                            anchor: 'end',
+                            font: {
+                                size: 20,
+                                weight: 'bold'
+                            },
                         }
                     },
+                    title: {
+                        display: true,
+                        text: `Nombre d'appels par commande sur ${message.guild.name}`,
+                        fontSize: 40,
+                        fontStyle: 'bold',
+                    }
+                    ,
+                    legend: {
+                        display: false
+                    },
                     scales: {
-                        x: {
+                        xAxes: [{
                             ticks: {
-                                font : {
-                                    size: 20
-                                }
+                                fontSize: 20
                             }
-                        },
-                        y: {
+                        }],
+                        yAxes: [{
                             ticks: {
-                                font : {
-                                    size: 40
-                                }
+                                fontSize: 40
                             }
-                        }
+                        }]
                     }
                 }
             };
         } else if (args[0] === 'members') {
-            canvasRenderService = new ChartJSNodeCanvas({ width, height });
+            canvasRenderService = new ChartJSNodeCanvas({
+                width,
+                height,
+                plugins: { requireLegacy: ['chartjs-plugin-datalabels'] }
+            });
             configuration = {
                 type: 'bar',
                 data: {
-                    labels: totalUserCommandsCounts_.map(el=>message.guild.members.cache.get(el[0]).user.username),
+                    labels: totalUserCommandsCounts_.map(el => message.guild.members.cache.get(el[0]).user.username),
                     datasets: [{
                         label: 'Nombre d\'appel',
                         data: totalUserCommandsCounts_.map(x => x[1]),
-                        backgroundColor:`hsl(${barColor},100%,70%)`,
-                        borderColor:`hsl(${barColor},100%,25%)`,
+                        backgroundColor: `hsl(${barColor},100%,70%)`,
+                        borderColor: `hsl(${barColor},100%,25%)`,
                         borderWidth: 3,
-                        borderRadius:10,
+                        // borderRadius: 10,
                     }]
                 },
                 plugins: [bg],
                 options: {
                     plugins: {
-                        title : {
-                            display: true,
-                            text:`Nombre d'appels par membre de ${message.guild.name}`,
-                            font : {
-                                weight : 'bold',
-                                size: 40
-                            }
-                        },
-                        legend: {
-                            display: false
+                        datalabels: {
+                            backgroundColor: function (context) {
+                                col = context.dataset.backgroundColor.slice(0, context.dataset.backgroundColor.length - 4)
+                                col = col + '85%)';
+                                return col;
+                            },
+                            borderColor: function (context) {
+                                return context.dataset.borderColor;
+                            },
+                            borderRadius: 25,
+                            borderWidth: 2,
+                            color: function (context) {
+                                return context.dataset.borderColor;
+                            },
+                            anchor: 'end',
+                            font: {
+                                size: 20,
+                                weight: 'bold'
+                            },
                         }
                     },
+
+                    title: {
+                        display: true,
+                        text: `Nombre d'appels par membre de ${message.guild.name}`,
+                        fontStyle: 'bold',
+                        fontSize: 40
+                    },
+                    legend: {
+                        display: false
+                    },
                     scales: {
-                        x: {
+                        xAxes: [{
                             ticks: {
-                                font : {
-                                    size: 20
-                                }
+                                fontSize: 20
                             }
-                        },
-                        y: {
+                        }],
+                        yAxes: [{
                             ticks: {
-                                font : {
-                                    size: 40
-                                }
+                                fontSize: 40
                             }
-                        }
+                        }]
                     }
                 }
             };
         }
         else {
             const commandName = args[0].toLowerCase();
-		    const command = message.client.commands.get(commandName)
-			|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+            const command = message.client.commands.get(commandName)
+                || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
             if (!command) return message.channel.send(`Je n'ai pas de face avec le nom ou l'alias \`${commandName}\` ! (N\'hésite pas à tenter \`!help stats\`)`);
 
-            commandStats = totalCommandsCounts_.find(e=>e[0]===command.name)[1][1];
+            commandStats = totalCommandsCounts_.find(e => e[0] === command.name)[1][1];
             // console.log(commandStats)
-            canvasRenderService = new ChartJSNodeCanvas({ 
-                width:height, 
-                height:height, 
-                // plugins: {
-                //     requireLegacy: ['chartjs-plugin-datalabels']
-                // }
+            canvasRenderService = new ChartJSNodeCanvas({
+                width: height,
+                height: height,
+                plugins: {
+                    requireLegacy: ['chartjs-plugin-datalabels'],
+                }
             });
             configuration = {
                 type: 'doughnut',
                 data: {
-                    labels: commandStats.map(el=>message.guild.members.cache.get(el[0]).user.username),
+                    labels: commandStats.map(el => message.guild.members.cache.get(el[0]).user.username),
                     datasets: [{
                         label: 'Nombre d\'appel',
                         data: commandStats.map(x => x[1]),
-                        // backgroundColor:`hsl(${barColor},100%,70%)`,
-                        // borderColor:`hsl(${barColor},100%,25%)`,
+                        backgroundColor: [...Array(commandStats.length).keys()].map(n => `hsl(${360 * n / commandStats.length},100%,70%)`),
+                        borderColor: [...Array(commandStats.length).keys()].map(n => `hsl(${360 * n / commandStats.length},100%,25%)`),
                         borderWidth: 3,
-                        borderRadius:10,
+                        // borderRadius: 10,
                     }]
                 },
                 plugins: [bg],
                 options: {
                     plugins: {
-                        title : {
-                            display: true,
-                            text:`Répartition des appels de ${command.name} sur ${message.guild.name}`,
-                            font : {
-                                weight : 'bold',
-                                size: 40
-                            }
-                        },
-                        legend: {
-                            display: true,
-                            labels:{
-                                font : {
-                                    size: 30
-                                }
-                            }
-                        },
-                        // labels: {
-                        //     render: 'percentage',
-                        //     fontColor: '#fff',
-                        // }
+                        datalabels: {
+                            backgroundColor: [...Array(commandStats.length).keys()].map(n => `hsl(${360 * n / commandStats.length},100%,85%)`),
+                            borderColor: function (context) {
+                                return context.dataset.borderColor;
+                            },
+                            borderRadius: 25,
+                            borderWidth: 2,
+                            color: function (context) {
+                                return context.dataset.borderColor;
+                            },
+                            formatter: function (value, context) {
+                                return `${value} - ${Math.round(100 * (value / context.dataset.data.reduce((a, b) => a + b, 0)))} %`;
+                            },
+                            anchor: 'start',
+                            font: {
+                                size: 20,
+                                weight: 'bold'
+                            },
+                        }
                     },
-                }
-            };
+                    layout: {
+                        padding: 50
+                    },
+                    title: {
+                        display: true,
+                        text: `Répartition des appels de ${command.name} sur ${message.guild.name}`,
+                        fontStyle: 'bold',
+                        fontSize: 40
+                    },
+                    legend: {
+                        display: true,
+                        labels: {
+                            fontSize: 30
+                        }
+                    },
 
+                },
+                centerText: {
+                    display: true,
+                    text: `${commandStats.map(x => x[1]).reduce((a, b) => a + b, 0)} appel${commandStats.map(x => x[1]).reduce((a, b) => a + b, 0)>1?'s':''}`
+                }
+            }
         }
         const image = await canvasRenderService.renderToBuffer(configuration);
         message.channel.send({
